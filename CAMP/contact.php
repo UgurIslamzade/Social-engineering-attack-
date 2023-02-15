543f080550eee23f869fff4ba4061b44730aae0d
Ugur <115311075+UgurIslamzade@users.noreply.github.com>
2023-02-15 18:10:19 +0400

    Phising
    Instagram Facebook Linkedin Google login page

---
+++ CAMP/contact.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact us</title>
+    <link rel="stylesheet" href="stylenav.css">
+    <link rel="stylesheet" href="contact.css">
+</head>
+<body>
+<nav>
+  <div class="navicon">
+    <div></div>
+  </div>
+  <a href="index.php">Home</a>
+  <a href="contact.php">Contact us</a>
+  <a href="about.php">About</a>
+</nav>
+<div class="container">
+        <div class="row">
+          <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="our-team">
+              <div class="picture">
+                <img class="img-fluid" src="Ugur.jpg">
+              </div>
+              <div class="team-content">
+                <h3 class="name">Ughur Islamzada</h3>
+                <h4 class="title">Team Leader</h4>
+              </div>
+              <ul class="social">
+                <li><a href="https://www.facebook.com/profile.php?id=100005654927489" class="fa fa-facebook" aria-hidden="true"></a></li>
+                <li><a href="https://instagram.com/ugurr19.03?igshid=ZDdkNTZiNTM=" class="fa fa-instagram" aria-hidden="true"></a></li>
+                <li><a href="https://www.linkedin.com/in/ugur-islamzade/" class="fa fa-linkedin" aria-hidden="true"></a></li>
+                <li><a href="mailto:ugur.islamzade@ascce.edu.az" class="fa fa-outlook" aria-hidden="true"></a></li>
+              </ul>
+            </div>
+          </div>
+              <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="our-team">
+              <div class="picture">
+                <img class="img-fluid" src="hum.jpg">
+              </div>
+              <div class="team-content">
+                <h3 class="name">Humbat Abbaszada</h3>
+                <h4 class="title">Backend Developer</h4>
+              </div>
+              <ul class="social">
+                <li><a href="https://www.facebook.com/humbat.abbaszade.9?mibextid=LQQJ4d" class="fa fa-facebook" aria-hidden="true"></a></li>
+                <li><a href="https://instagram.com/humbet__14?igshid=YmMyMTA2M2Y=" class="fa fa-instagram" aria-hidden="true"></a></li>
+                <li><a href="https://www.linkedin.com/in/humbet-abbaszade-a5108625b " class="fa fa-linkedin" aria-hidden="true"></a></li>
+                <li><a href="mailto:humbet.abbaszade@ascca.edu.az" class="fa fa-outlook" aria-hidden="true"></a></li>
+              </ul>
+            </div>
+          </div>
+              <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="our-team">
+              <div class="picture">
+                <img class="img-fluid" src="elnur1.jpg">
+              </div>
+              <div class="team-content">
+                <h3 class="name">Elnur Aliyev</h3>
+                <h4 class="title">Frontend Developer</h4>
+              </div>
+              <ul class="social">
+                <li><a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-outlook" aria-hidden="true"></a></li>
+                <li><a href="https://instagram.com/_elnur.aliyev07?igshid=YmMyMTA2M2Y=" class="fa fa-instagram" aria-hidden="true"></a></li>
+                <li><a href="mailto:eliyev.elnur@ascca.edu.az" class="fa fa-outlook" aria-hidden="true"></a></li>
+              </ul>
+            </div>
+          </div>
+              <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="our-team">
+              <div class="picture">
+                <img class="img-fluid" src="Kenan.jpg">
+              </div>
+              <div class="team-content">
+                <h3 class="name">Kanan Akbarov</h3>
+                <h4 class="title">Frontend Developer</h4>
+              </div>
+              <ul class="social">
+                <li><a href="https://m.facebook.com/100086287162261/" class="fa fa-facebook" aria-hidden="true"></a></li>
+                <li><a href="https://www.instagram.com/akkbrvh/" class="fa fa-instagram" aria-hidden="true"></a></li>
+                <li><a href="mailto:kenan.ekberov@ascca.edu.az" class="fa fa-outlook" aria-hidden="true"></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+</body>
+</html>
